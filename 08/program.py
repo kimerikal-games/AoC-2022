34f48ad8453c27e3d3e1a9288b0c508455b47e0a
kimerikal <kimerikal.games@gmail.com>
2022-12-08 20:53:29 +0900

    Add day 8

---
+++ 08/program.py
@@ -0,0 +1,26 @@
+"""
+Advent of Code 2022 - Day 8: Treetop Tree House
+Author: kimerikal <kimerikal.games@gmail.com>
+"""
+import sys
+
+
+def main() -> int:
+    sys.stdin
+
+    print("Part 1:", part1())
+    print("Part 2:", part2())
+
+    return 0
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
