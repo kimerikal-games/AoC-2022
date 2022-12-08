cb13221e868cce482f496034d7fbe2d87d44b5eb
kimerikal <kimerikal.games@gmail.com>
2022-12-08 20:52:14 +0900

    Add solution for day 7

--- 07/program.py
+++ 07/program.py
@@ -3,10 +3,11 @@ Advent of Code 2022 - Day 7: No Space Left On Device
 Author: kimerikal <kimerikal.games@gmail.com>
 """
 import sys
+from collections import defaultdict
 
 
 def main() -> int:
-    commands = sys.stdin.read().split('\n')
+    commands = sys.stdin.read().split("\n")
 
     print("Part 1:", part1(commands))
     print("Part 2:", part2(commands))
@@ -15,12 +16,53 @@ def main() -> int:
 
 
 def part1(commands: list[str]) -> int:
-    for command in commands:
-        print(command)
+    dirsizes = solve(commands)
+    return sum(x for x in dirsizes.values() if x <= 100000)
 
 
 def part2(commands: list[str]) -> int:
-    pass
+    dirsizes = solve(commands)
+    total_size = dirsizes[""]
+    needed_size = 30000000 - (70000000 - total_size)
+    return min(x for x in dirsizes.values() if x >= needed_size)
+
+
+def solve(commands: list[str]) -> dict[str, int]:
+    command_no = 0
+    wd: list[str] = []
+    dirsizes: defaultdict[str, int] = defaultdict(int)
+
+    while command_no < len(commands):
+        command = commands[command_no]
+
+        match command.split():
+            case ["$", "cd", "/"]:
+                wd.clear()
+                wd.append("")
+            case ["$", "cd", ".."]:
+                wd.pop()
+            case ["$", "cd", dirname]:
+                wd.append(wd[-1] + "/" + dirname)
+            case ["$", "ls"]:
+                j = command_no
+                total_size = 0
+                while j + 1 < len(commands) and not commands[j + 1].startswith("$"):
+                    j += 1
+                    match commands[j].split():
+                        case ["dir", dirname]:
+                            pass
+                        case [size_raw, _]:
+                            total_size += int(size_raw)
+                        case _:
+                            pass
+                for d in wd:
+                    dirsizes[d] += total_size
+                command_no = j
+            case _:
+                pass
+
+        command_no += 1
+    return dirsizes
 
 
 if __name__ == "__main__":
